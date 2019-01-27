c8da710b1561ff6d229c882a63873f07e476e2f5
mitchellhamilton <mitchell@hamil.town>
2019-01-27 15:50:11 +1000

    Use hooks

--- packages/admin-ui/client/pages/List/ListDetails.js
+++ packages/admin-ui/client/pages/List/ListDetails.js
@@ -1,6 +1,6 @@
 /** @jsx jsx */
-import { jsx } from '@emotion/core';
-import { Component, createRef, Fragment, useEffect, useState } from 'react';
+import { jsx, Global } from '@emotion/core';
+import { Fragment, useEffect, useState, useRef } from 'react';
 import styled from '@emotion/styled';
 import { withRouter } from 'react-router-dom';
 
@@ -95,18 +95,6 @@ type Props = {
   skip: number,
   sortBy: SortByType,
 };
-type State = {
-  isFullWidth: boolean,
-  selectedItems: Array<Object>,
-  showCreateModal: boolean,
-};
-
-function bodyUserSelect(val) {
-  document.body.style.WebkitUserSelect = val;
-  document.body.style.MozUserSelect = val;
-  document.body.style.msUserSelect = val;
-  document.body.style.userSelect = val;
-}
 
 function useShiftIsDown() {
   let [shiftIsDown, setShiftIsDown] = useState(false);
@@ -170,308 +158,259 @@ function NoResultsMessage({ filters, itemsCount, list, search, currentPage, hand
   return null;
 }
 
-class ListDetails extends Component<Props, State> {
-  state = {
-    isFullWidth: false,
-    selectedItems: [],
-    showCreateModal: false,
-    searchValue: this.props.search,
-  };
-  lastChecked = null;
-  shiftIsDown = false;
+const ListDetails = ({
+  adminPath,
+  currentPage,
+  fields,
+  filters,
+  handleFieldChange,
+  handleFilterAdd,
+  handleFilterRemove,
+  handleFilterRemoveAll,
+  handleFilterUpdate,
+  handlePageChange,
+  handlePageReset,
+  handleSortChange,
+  items,
+  itemsCount,
+  itemsErrors,
+  list,
+  pageSize,
+  query,
+  sortBy,
+  search,
+  history,
+  ...props
+}: Props) => {
+  let [isFullWidth, setIsFullWidth] = useState(false);
+  let [selectedItems, setSelectedItems] = useState([]);
+  let [showCreateModal, setShowCreateModal] = useState(false);
+  let [searchValue, setSearchValue] = useState(search);
+  let shiftIsDown = useShiftIsDown();
+  let lastCheckedRef = useRef(null);
+  let sortPopoutRef = useRef(null);
+  let searchInputRef = useRef(null);
+  const searchId = 'ks-list-search-input';
 
-  // ==============================
-  // Refs
-  // ==============================
-
-  sortPopoutRef = createRef();
-
-  componentDidMount() {
-    document.addEventListener('keydown', this.handleKeyDown);
-    document.addEventListener('keyup', this.handleKeyUp);
-  }
-  componentWillUnmount() {
-    document.removeEventListener('keydown', this.handleKeyDown);
-    document.removeEventListener('keyup', this.handleKeyUp);
-  }
-
-  handleKeyDown = event => {
-    if (event.key === 'Shift') {
-      if (this.state.selectedItems.length > 0) {
-        bodyUserSelect('none');
-      }
-      this.shiftIsDown = true;
-    }
-  };
-  handleKeyUp = event => {
-    if (event.key === 'Shift') {
-      if (this.state.selectedItems.length > 0) {
-        bodyUserSelect(null);
-      }
-      this.shiftIsDown = false;
-    }
-  };
-
-  render() {
-    const {
-      adminPath,
-      currentPage,
-      fields,
-      filters,
-      handleFieldChange,
-      handleFilterAdd,
-      handleFilterRemove,
-      handleFilterRemoveAll,
-      handleFilterUpdate,
-      handlePageChange,
-      handlePageReset,
-      handleSortChange,
-      items,
-      itemsCount,
-      itemsErrors,
-      list,
-      pageSize,
-      query,
-      sortBy,
-      search,
-      history,
-    } = this.props;
-    const { isFullWidth, selectedItems, showCreateModal, searchValue } = this.state;
-
-    const searchId = 'ks-list-search-input';
-
-    let closeCreateModal = () => this.setState({ showCreateModal: false });
-    let openCreateModal = () => this.setState({ showCreateModal: true });
-
-    // ==============================
-    // Search
-    // ==============================
-
-    let handleSearchChange = ({ target: { value } }) => {
-      this.setState({ searchValue: value }, () => {
-        this.props.handleSearchChange(value);
-      });
-    };
-    let handleSearchClear = () => {
-      this.setState({ searchValue: '' });
-      this.props.handleSearchClear();
-      this.searchInput.focus();
-    };
-    let handleReset = () => {
-      this.setState({ searchValue: '' });
-      this.props.handleReset();
-    };
-    let handleSearchSubmit = event => {
-      event.preventDefault();
-      this.props.handleSearchSubmit();
-    };
-
-    // ==============================
-    // Management
-    // ==============================
-
-    let handleItemSelect = (itemId: string) => {
-      let selectedItems = this.state.selectedItems.slice(0);
-
-      if (this.shiftIsDown && this.lastChecked) {
-        const itemIds = this.props.items.map(i => i.id);
-        const from = itemIds.indexOf(itemId);
-        const to = itemIds.indexOf(this.lastChecked);
-        const start = Math.min(from, to);
-        const end = Math.max(from, to) + 1;
-
-        itemIds
-          .slice(start, end)
-          .filter(id => id !== this.lastChecked)
-          .forEach(id => {
-            if (!selectedItems.includes(this.lastChecked)) {
-              selectedItems = selectedItems.filter(existingId => existingId !== id);
-            } else {
-              selectedItems.push(id);
-            }
-          });
-
-        // lazy ensure unique
-        const uniqueItems = [...new Set(selectedItems)];
-        this.setState({ selectedItems: uniqueItems });
-      } else {
-        if (selectedItems.includes(itemId)) {
-          selectedItems = selectedItems.filter(existingId => existingId !== itemId);
-        } else {
-          selectedItems.push(itemId);
+  return (
+    <Fragment>
+      <Global
+        styles={
+          shiftIsDown && selectedItems.length > 0
+            ? {
+                body: { userSelect: 'none' },
+              }
+            : null
         }
-
-        this.setState({ selectedItems });
-      }
-
-      this.lastChecked = itemId;
-    };
-    let handleItemSelectAll = (selectedItems: Array<string>) => {
-      this.setState({ selectedItems });
-    };
-    let onDeleteSelectedItems = () => {
-      if (query.refetch) query.refetch();
-      this.setState({ selectedItems: [] });
-    };
-    let onCreate = ({ data }) => {
-      let id = data[list.gqlNames.createMutationName].id;
-      history.push(`${adminPath}/${list.path}/${id}`);
-    };
-
-    let toggleFullWidth = () => {
-      this.setState(state => ({ isFullWidth: !state.isFullWidth }));
-    };
-    return (
-      <Fragment>
-        <main>
-          <ContainerQuery>
-            {({ width }) => (
-              <Container isFullWidth={isFullWidth}>
-                <Title as="h1" margin="both">
-                  {itemsCount > 0 ? list.formatCount(itemsCount) : list.plural}
-                  <span>, by</span>
-                  <Popout
-                    innerRef={this.sortPopoutRef}
-                    headerTitle="Sort"
-                    footerContent={
-                      <Note>
-                        Hold <Kbd>alt</Kbd> to toggle ascending/descending
-                      </Note>
-                    }
-                    target={
-                      <SortButton>
-                        {sortBy.field.label.toLowerCase()}
-                        <DisclosureArrow size="0.2em" />
-                      </SortButton>
-                    }
-                  >
-                    <SortSelect
-                      popoutRef={this.sortPopoutRef}
-                      fields={list.fields}
-                      onChange={handleSortChange}
-                      value={sortBy}
-                    />
-                  </Popout>
-                </Title>
-
-                <FlexGroup growIndexes={[0]}>
-                  <Search
-                    isFetching={query.loading}
-                    onClear={handleSearchClear}
-                    onSubmit={handleSearchSubmit}
-                    hasValue={searchValue && searchValue.length}
-                  >
-                    <A11yText tag="label" htmlFor={searchId}>
-                      Search {list.plural}
-                    </A11yText>
-                    <Input
-                      autoCapitalize="off"
-                      autoComplete="off"
-                      autoCorrect="off"
-                      id={searchId}
-                      onChange={handleSearchChange}
-                      placeholder="Search"
-                      name="item-search"
-                      value={searchValue}
-                      type="text"
-                      ref={el => (this.searchInput = el)}
-                    />
-                  </Search>
-                  <AddFilterPopout
-                    existingFilters={filters}
+      />
+      <main>
+        <ContainerQuery>
+          {({ width }) => (
+            <Container isFullWidth={isFullWidth}>
+              <Title as="h1" margin="both">
+                {itemsCount > 0 ? list.formatCount(itemsCount) : list.plural}
+                <span>, by</span>
+                <Popout
+                  innerRef={sortPopoutRef}
+                  headerTitle="Sort"
+                  footerContent={
+                    <Note>
+                      Hold <Kbd>alt</Kbd> to toggle ascending/descending
+                    </Note>
+                  }
+                  target={
+                    <SortButton>
+                      {sortBy.field.label.toLowerCase()}
+                      <DisclosureArrow size="0.2em" />
+                    </SortButton>
+                  }
+                >
+                  <SortSelect
+                    popoutRef={sortPopoutRef}
                     fields={list.fields}
-                    onChange={handleFilterAdd}
+                    onChange={handleSortChange}
+                    value={sortBy}
                   />
-                  <Popout buttonLabel="Columns" headerTitle="Columns">
-                    <ColumnSelect
-                      fields={list.fields}
-                      onChange={handleFieldChange}
-                      removeIsAllowed={fields.length > 1}
-                      value={fields}
-                    />
-                  </Popout>
+                </Popout>
+              </Title>
 
-                  {list.access.create ? (
-                    <IconButton appearance="create" icon={PlusIcon} onClick={openCreateModal}>
-                      Create
-                    </IconButton>
-                  ) : null}
-                  <MoreDropdown
-                    width={width}
-                    isFullWidth={isFullWidth}
-                    onReset={handleReset}
-                    onFullWidthToggle={toggleFullWidth}
+              <FlexGroup growIndexes={[0]}>
+                <Search
+                  isFetching={query.loading}
+                  onClear={() => {
+                    setSearchValue('');
+                    props.handleSearchClear();
+                    searchInputRef.current.focus();
+                  }}
+                  onSubmit={event => {
+                    event.preventDefault();
+                    props.handleSearchSubmit();
+                  }}
+                  hasValue={searchValue && searchValue.length}
+                >
+                  <A11yText tag="label" htmlFor={searchId}>
+                    Search {list.plural}
+                  </A11yText>
+                  <Input
+                    autoCapitalize="off"
+                    autoComplete="off"
+                    autoCorrect="off"
+                    id={searchId}
+                    onChange={({ target: { value } }) => {
+                      setSearchValue(value);
+                      props.handleSearchChange(searchValue);
+                    }}
+                    placeholder="Search"
+                    name="item-search"
+                    value={searchValue}
+                    type="text"
+                    ref={searchInputRef}
                   />
-                </FlexGroup>
-
-                <ActiveFilters
-                  filterList={filters}
-                  onUpdate={handleFilterUpdate}
-                  onRemove={handleFilterRemove}
-                  onClear={handleFilterRemoveAll}
+                </Search>
+                <AddFilterPopout
+                  existingFilters={filters}
+                  fields={list.fields}
+                  onChange={handleFilterAdd}
                 />
-
-                <ManageToolbar isVisible={!!itemsCount}>
-                  {selectedItems.length ? (
-                    <Management
-                      list={list}
-                      onDeleteMany={onDeleteSelectedItems}
-                      // onUpdateMany={onUpdate}
-                      pageSize={pageSize}
-                      selectedItems={selectedItems}
-                      totalItems={itemsCount}
-                    />
-                  ) : (
-                    <Pagination
-                      isLoading={query.loading}
-                      currentPage={currentPage}
-                      itemsCount={itemsCount}
-                      list={list}
-                      onChangePage={handlePageChange}
-                      pageSize={pageSize}
-                    />
-                  )}
-                </ManageToolbar>
-              </Container>
-            )}
-          </ContainerQuery>
-
-          <CreateItemModal
-            isOpen={showCreateModal}
-            list={list}
-            onClose={closeCreateModal}
-            onCreate={onCreate}
-          />
-
-          <Container isFullWidth={isFullWidth}>
-            {items ? (
-              <ListTable
-                adminPath={adminPath}
-                fields={fields}
-                isFullWidth={isFullWidth}
-                items={items}
-                itemsErrors={itemsErrors}
-                list={list}
-                onChange={query.refetch}
-                onSelect={handleItemSelect}
-                onSelectAll={handleItemSelectAll}
-                handleSortChange={handleSortChange}
-                sortBy={sortBy}
-                selectedItems={selectedItems}
-                noResultsMessage={
-                  <NoResultsMessage
-                    {...{ filters, itemsCount, list, search, currentPage, handlePageReset }}
+                <Popout buttonLabel="Columns" headerTitle="Columns">
+                  <ColumnSelect
+                    fields={list.fields}
+                    onChange={handleFieldChange}
+                    removeIsAllowed={fields.length > 1}
+                    value={fields}
                   />
-                }
+                </Popout>
+
+                {list.access.create ? (
+                  <IconButton
+                    appearance="create"
+                    icon={PlusIcon}
+                    onClick={() => setShowCreateModal(true)}
+                  >
+                    Create
+                  </IconButton>
+                ) : null}
+                <MoreDropdown
+                  width={width}
+                  isFullWidth={isFullWidth}
+                  onReset={() => {
+                    setSearchValue('');
+                    props.handleReset();
+                  }}
+                  onFullWidthToggle={() => {
+                    setIsFullWidth(!isFullWidth);
+                  }}
+                />
+              </FlexGroup>
+
+              <ActiveFilters
+                filterList={filters}
+                onUpdate={handleFilterUpdate}
+                onRemove={handleFilterRemove}
+                onClear={handleFilterRemoveAll}
               />
-            ) : (
-              <PageLoading />
-            )}
-          </Container>
-        </main>
-      </Fragment>
-    );
-  }
-}
+
+              <ManageToolbar isVisible={!!itemsCount}>
+                {selectedItems.length ? (
+                  <Management
+                    list={list}
+                    onDeleteMany={() => {
+                      if (query.refetch) query.refetch();
+                      setSelectedItems([]);
+                    }}
+                    // onUpdateMany={onUpdate}
+                    pageSize={pageSize}
+                    selectedItems={selectedItems}
+                    totalItems={itemsCount}
+                  />
+                ) : (
+                  <Pagination
+                    isLoading={query.loading}
+                    currentPage={currentPage}
+                    itemsCount={itemsCount}
+                    list={list}
+                    onChangePage={handlePageChange}
+                    pageSize={pageSize}
+                  />
+                )}
+              </ManageToolbar>
+            </Container>
+          )}
+        </ContainerQuery>
+
+        <CreateItemModal
+          isOpen={showCreateModal}
+          list={list}
+          onClose={() => setShowCreateModal(false)}
+          onCreate={({ data }) => {
+            let id = data[list.gqlNames.createMutationName].id;
+            history.push(`${adminPath}/${list.path}/${id}`);
+          }}
+        />
+
+        <Container isFullWidth={isFullWidth}>
+          {items ? (
+            <ListTable
+              adminPath={adminPath}
+              fields={fields}
+              isFullWidth={isFullWidth}
+              items={items}
+              itemsErrors={itemsErrors}
+              list={list}
+              onChange={query.refetch}
+              onSelect={(itemId: string) => {
+                let newSelectedItems = selectedItems.slice(0);
+
+                if (shiftIsDown && lastCheckedRef.current) {
+                  const itemIds = items.map(i => i.id);
+                  const from = itemIds.indexOf(itemId);
+                  const to = itemIds.indexOf(lastCheckedRef.current);
+                  const start = Math.min(from, to);
+                  const end = Math.max(from, to) + 1;
+
+                  itemIds
+                    .slice(start, end)
+                    .filter(id => id !== lastCheckedRef.current)
+                    .forEach(id => {
+                      if (!newSelectedItems.includes(lastCheckedRef.current)) {
+                        newSelectedItems = newSelectedItems.filter(existingId => existingId !== id);
+                      } else {
+                        newSelectedItems.push(id);
+                      }
+                    });
+
+                  // lazy ensure unique
+                  const uniqueItems = [...new Set(newSelectedItems)];
+                  setSelectedItems(uniqueItems);
+                } else {
+                  if (newSelectedItems.includes(itemId)) {
+                    newSelectedItems = newSelectedItems.filter(existingId => existingId !== itemId);
+                  } else {
+                    newSelectedItems.push(itemId);
+                  }
+
+                  setSelectedItems(newSelectedItems);
+                }
+
+                lastCheckedRef.current = itemId;
+              }}
+              onSelectAll={setSelectedItems}
+              handleSortChange={handleSortChange}
+              sortBy={sortBy}
+              selectedItems={selectedItems}
+              noResultsMessage={
+                <NoResultsMessage
+                  {...{ filters, itemsCount, list, search, currentPage, handlePageReset }}
+                />
+              }
+            />
+          ) : (
+            <PageLoading />
+          )}
+        </Container>
+      </main>
+    </Fragment>
+  );
+};
 
 export default withRouter(ListDetails);
